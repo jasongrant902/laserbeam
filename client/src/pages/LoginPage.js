82a7ac33363e16b0c1ff5fb2deadde543873b6f7
Jason Grant <141342785+jasongrant902@users.noreply.github.com>
2025-04-01 14:27:09 -0600

    Added show password button to login and register pages. Increased number of data fields taken on user registration. Minor updates to page styling.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -6,18 +6,19 @@ export default function LoginPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [redirect, setRedirect] = useState(false);
-  const {setUserInfo} = useContext(UserContext);
-  async function login(ev){
+  const [showPassword, setShowPassword] = useState(false);
+  const { setUserInfo } = useContext(UserContext);
+  async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('http://localhost:4000/login', {
-        method: 'POST',
-        body: JSON.stringify({username, password}),
-        headers: {'Content-Type':'application/json'},
-        credentials: 'include',
+    const response = await fetch("http://localhost:4000/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
     });
 
     if (response.ok) {
-      response.json().then(userInfo => {
+      response.json().then((userInfo) => {
         setUserInfo(userInfo);
         setRedirect(true);
       });
@@ -27,7 +28,7 @@ export default function LoginPage() {
   }
 
   if (redirect) {
-    return <Navigate to={'/'}></Navigate>
+    return <Navigate to={"/"}></Navigate>;
   }
 
   return (
@@ -35,18 +36,67 @@ export default function LoginPage() {
       <h1>Login</h1>
       <input
         type="text"
-        placeholder="username"
+        placeholder="Username"
         value={username}
         onChange={(ev) => setUsername(ev.target.value)}
       ></input>
-      <br></br>
+      <div className="password-container">
       <input
-        type="password"
-        placeholder="password"
+        type={showPassword ? "text" : "password"}
+        placeholder="Password"
         value={password}
         onChange={(ev) => setPassword(ev.target.value)}
       ></input>
-      <br></br>
+      <button
+        type="button"
+        className="show-btn"
+        onMouseDown={() => setShowPassword(true)}
+        onMouseUp={() => setShowPassword(false)}
+        onMouseLeave={() => setShowPassword(false)}
+      >
+        {showPassword ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="eye-icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 
+          7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="eye-icon"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 
+  2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 
+  10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 
+  0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+            />
+          </svg>
+        )}
+      </button>
+      </div>
+      <br />
       <button>Login</button>
     </form>
   );
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,21 +1,28 @@
 import { useState } from "react";
+import ReactFlagsSelect from "react-flags-select";
 
 export default function RegisterPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [passwordHint, setPasswordHint] = useState("");
+  const [nationality, setNationality] = useState(null);
 
   async function register(ev) {
     ev.preventDefault();
-      const response = await fetch('http://localhost:4000/register', {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-      if (response.status !== 200) {
-        alert("Registration failed");
-      } else {
-        alert("Registration successful")
-      }
+    const response = await fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, password, firstName, lastName, email, nationality, passwordHint }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status !== 200) {
+      alert("Registration failed");
+    } else {
+      alert("Registration successful");
+    }
   }
 
   return (
@@ -23,17 +30,99 @@ export default function RegisterPage() {
       <h1>Sign Up For Laserbeam</h1>
       <input
         type="text"
-        placeholder="username"
+        placeholder="Username"
         value={username}
         onChange={(ev) => setUsername(ev.target.value)}
       />
-      <br></br>
+      <div className="password-container">
+        <input
+          type={showPassword ? "text" : "password"}
+          placeholder="Password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        ></input>
+        <button
+          type="button"
+          className="show-btn"
+          onMouseDown={() => setShowPassword(true)}
+          onMouseUp={() => setShowPassword(false)}
+          onMouseLeave={() => setShowPassword(false)}
+        >
+          {showPassword ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="eye-icon"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 
+          7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="eye-icon"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 
+  2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 
+  10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 
+  0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+              />
+            </svg>
+          )}
+        </button>
+      </div>
       <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(ev) => setPassword(ev.target.value)}
+        type="passwordhint"
+        placeholder="Password hint (Optional)"
+        value={passwordHint}
+        onChange={(e) => setPasswordHint(e.target.value)}
       />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="firstname"
+        placeholder="First Name"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="lastname"
+        placeholder="Last Name (Optional)"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <div className="flags-wrapper">
+      <ReactFlagsSelect
+        selected={nationality}
+        onSelect={(code) => setNationality(code)}
+        searchable
+        placeholder="Select Nationality (Optional)"
+      />
+      </div>
       <br></br>
       <button>Register</button>
     </form>
